c8db1a5352b48c31ee3ca91c704cdc3329d5f4a7
David Vega <david@izarus.cl>
2014-05-22 01:15:53 -0400

    List: use of glyph icon on boolean field.

--- data/generator/sfDoctrineModule/bootstrap/template/templates/_list_field_boolean.php
+++ data/generator/sfDoctrineModule/bootstrap/template/templates/_list_field_boolean.php
@@ -1,5 +1,5 @@
 [?php if ($value): ?]
-  [?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/tick.png', array('alt' => __('Checked', array(), 'sf_admin'), 'title' => __('Checked', array(), 'sf_admin'))) ?]
+  <span class="glyphicon glyphicon-ok text-success"></span>
 [?php else: ?]
-  &nbsp;
+  <span class="glyphicon glyphicon-remove text-danger"></span>
 [?php endif; ?]
