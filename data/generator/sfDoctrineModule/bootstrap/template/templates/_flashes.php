5875971f2e345d48f391141ae0f460967d389b0a
Alex Arenas <alex@izarus.cl>
2014-10-23 16:35:16 -0300

    Mensajes en flashes ahora pueden tener html

--- data/generator/sfDoctrineModule/bootstrap/template/templates/_flashes.php
+++ data/generator/sfDoctrineModule/bootstrap/template/templates/_flashes.php
@@ -1,34 +1,34 @@
 [?php if ($sf_user->hasFlash('notice')): ?]
   <div class="alert alert-success">
     <a href="#" class="close fade" data-dismiss="alert">&times;</a>
-    [?php echo __($sf_user->getFlash('notice'), array(), 'sf_admin') ?]
+    [?php echo __($sf_data->getRaw('sf_user')->getFlash('notice'), array(), 'sf_admin') ?]
   </div>
 [?php endif; ?]
 
 [?php if ($sf_user->hasFlash('error')): ?]
   <div class="alert alert-danger">
     <a href="#" class="close fade" data-dismiss="alert">&times;</a>
-    [?php echo __($sf_user->getFlash('error'), array(), 'sf_admin') ?]
+    [?php echo __($sf_data->getRaw('sf_user')->getFlash('error'), array(), 'sf_admin') ?]
   </div>
 [?php endif; ?]
 
 [?php if ($sf_user->hasFlash('warning')): ?]
   <div class="alert alert-warning">
     <a href="#" class="close fade" data-dismiss="alert">&times;</a>
-    [?php echo __($sf_user->getFlash('warning'), array(), 'sf_admin') ?]
+    [?php echo __($sf_data->getRaw('sf_user')->getFlash('warning'), array(), 'sf_admin') ?]
   </div>
 [?php endif; ?]
 
 [?php if ($sf_user->hasFlash('success')): ?]
   <div class="alert alert-success">
     <a href="#" class="close fade" data-dismiss="alert">&times;</a>
-    [?php echo __($sf_user->getFlash('success'), array(), 'sf_admin') ?]
+    [?php echo __($sf_data->getRaw('sf_user')->getFlash('success'), array(), 'sf_admin') ?]
   </div>
 [?php endif; ?]
 
 [?php if ($sf_user->hasFlash('info')): ?]
   <div class="alert alert-info">
     <a href="#" class="close fade" data-dismiss="alert">&times;</a>
-    [?php echo __($sf_user->getFlash('info'), array(), 'sf_admin') ?]
+    [?php echo __($sf_data->getRaw('sf_user')->getFlash('info'), array(), 'sf_admin') ?]
   </div>
 [?php endif; ?]
