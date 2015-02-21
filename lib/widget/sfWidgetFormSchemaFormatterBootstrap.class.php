9800edcd1d10a534ed93eb80866640830a5b9c29
Alex Arenas <alex@izarus.cl>
2015-02-21 19:32:16 -0300

    Agregados control-type y control-name (reemplaza a form-group-name)

--- lib/widget/sfWidgetFormSchemaFormatterBootstrap.class.php
+++ lib/widget/sfWidgetFormSchemaFormatterBootstrap.class.php
@@ -3,7 +3,7 @@
 class sfWidgetFormSchemaFormatterBootstrap extends sfWidgetFormSchemaFormatter
 {
   protected
-    $rowFormat                 = "<div class=\"form-group form-group-%name% %error_class%\">\n  %label%\n  <div class=\"col-lg-%parentcols%\">%field%</div>\n<div class=\"col-lg-offset-%labelcols% col-lg-%fieldcols%\">%help%\n%error%</div>\n%hidden_fields%</div>\n",
+    $rowFormat                 = "<div class=\"form-group control-type-%type% control-name-%name% %error_class%\">\n  %label%\n  <div class=\"col-lg-%parentcols%\">%field%</div>\n<div class=\"col-lg-offset-%labelcols% col-lg-%fieldcols%\">%help%\n%error%</div>\n</div>\n%hidden_fields%\n",
     $helpFormat                = "<span class=\"help-block\">%help%</span>",
     $errorRowFormat            = "\n%errors%\n",
     $errorListFormatInARow     = "\n%errors%\n",
@@ -27,7 +27,7 @@ class sfWidgetFormSchemaFormatterBootstrap extends sfWidgetFormSchemaFormatter
           $field->setAttribute('class','form-control');
         }
       }
-      if ($field->hasOption('with_empty')) $field->setOption('with_empty',false);
+      if ($field->hasOption('with_empty')) $field->setOption('with_empty',false);      
     }
   }
 
@@ -68,7 +68,15 @@ class sfWidgetFormSchemaFormatterBootstrap extends sfWidgetFormSchemaFormatter
     @$doc->loadHTML($field);
     $xpath = new DOMXPath($doc);
     $pclass = $xpath->evaluate("string(//*/@data-parent-cols)");
-  
+    
+    $type = strtolower(str_replace(array('sfWidgetForm','Input'),'',get_class($this->widgetSchema[$this->name])));
+    if(get_class($this->widgetSchema[$this->name])==get_class($this) || empty($type))
+      $type = 'field';
+    
+    $name = $this->name;
+    $this->name = '';
+    $name = (empty($name))?'field':$name;   
+    
     return strtr($this->getRowFormat(), array(
       '%label%'         => $label,
       '%error_class%'   => count($errors) ? 'has-error' : '',
@@ -76,7 +84,8 @@ class sfWidgetFormSchemaFormatterBootstrap extends sfWidgetFormSchemaFormatter
       '%error%'         => $this->formatErrorsForRow($errors),
       '%help%'          => $this->formatHelp($help),
       '%hidden_fields%' => null === $hiddenFields ? '%hidden_fields%' : $hiddenFields,
-      '%name%'          => $this->name,
+      '%name%'          => $name,
+      '%type%'          =>  $type,
       '%parentcols%'     => (!empty($pclass))?$pclass:sfConfig::get('app_bootstrap_admin_fieldcols',8),
       '%labelcols%'     => sfConfig::get('app_bootstrap_admin_labelcols',4),
       '%fieldcols%'     => sfConfig::get('app_bootstrap_admin_fieldcols',8),
